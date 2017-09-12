5aea6f0421f0bcff437aac6a8327e931e0ed5376
Amadeu Andrade <amadeu.dev@gmail.com>
2017-09-12 21:24:15 +0100

    Added MapViewController skeleton

---
+++ RealmMapViewMVP/MapPresenter.swift
@@ -0,0 +1,15 @@
+//
+//  MapPresenter.swift
+//  RealmMapViewMVP
+//
+//  Created by Amadeu Andrade on 12/09/2017.
+//  Copyright © 2017 Amadeu Andrade. All rights reserved.
+//
+
+import Foundation
+
+protocol MapPresenterRepresentable { }
+
+class MapPresenter: MapPresenterRepresentable {
+
+}
---
+++ RealmMapViewMVP/MapViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MapViewController.swift
+//  RealmMapViewMVP
+//
+//  Created by Amadeu Andrade on 12/09/2017.
+//  Copyright © 2017 Amadeu Andrade. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+  
+  // MARK: - DEPENDENCIES
+  
+  var presenter: MapPresenterRepresentable!
+  
+  // MARK: - VIEW LIFE CYCLE
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+}
