86f704b871f2d5536cb1eb4474dbeaca5e1723dc
Amadeu Andrade <amadeu.dev@gmail.com>
2017-09-14 21:27:57 +0100

    Updated restaurant details view controller and presenter

--- RealmMapViewMVP/RestaurantDetailsPresenter.swift
+++ RealmMapViewMVP/RestaurantDetailsPresenter.swift
@@ -11,7 +11,15 @@ import CoreLocation
 import RealmSwiftSFRestaurantData
 
 protocol RestaurantDetailsPresenterRepresentable {
+  var restaurantName: String { get }
+  var restaurantAddress: String { get }
+  var restaurantCity: String { get }
+  var restaurantCoordinates: CLLocationCoordinate2D { get }
+  var restaurantPhone: String { get }
+  var restaurantRating: String { get }
+  var phoneEnabled: Bool { get }
   
+  func phonePressed()
 }
 
 struct RestaurantDetailsPresenter: RestaurantDetailsPresenterRepresentable {
@@ -20,13 +28,15 @@ struct RestaurantDetailsPresenter: RestaurantDetailsPresenterRepresentable {
   
   private let navigator: NavigatorRepresentable
   
-  // MARK: - PRIVATE PROPERTIES
+  // MARK: - OUTPUT PROPERTIES
   
-  private let restaurantName: String
-  private let restaurantAddress: String
-  private let restaurantCoordinates: CLLocationCoordinate2D
-  private let restaurantPhone: String?
-  private let restaurantRating: String
+  let restaurantName: String
+  let restaurantAddress: String
+  let restaurantCity: String
+  let restaurantCoordinates: CLLocationCoordinate2D
+  let restaurantPhone: String
+  let restaurantRating: String
+  let phoneEnabled: Bool
   
   // MARK: - INITIALIZER
   
@@ -34,12 +44,24 @@ struct RestaurantDetailsPresenter: RestaurantDetailsPresenterRepresentable {
     self.navigator = navigator
     restaurantName = restaurant.name?.capitalized ?? "Unknown name"
     restaurantAddress = restaurant.address?.capitalized ?? "Unknown address"
-    restaurantPhone = restaurant.phoneNumber
+    restaurantCity = restaurant.city?.capitalized ?? "Unknown city"
+    let hasPhone = (restaurant.phoneNumber != nil && restaurant.phoneNumber != "")
+    restaurantPhone = hasPhone ? restaurant.phoneNumber! : "--- --- ----"
+    phoneEnabled = hasPhone
     let coordinates = CLLocationCoordinate2D(latitude: restaurant.latitude, longitude: restaurant.longitude)
     restaurantCoordinates = coordinates
     let ratingCount = restaurant.inspections.count
     let rating = restaurant.inspections.reduce(0) { $0 + $1.score } / ratingCount
     restaurantRating = "\(rating)"
+  }
+  
+  // MARK: - METHODS
+  
+  func phonePressed() {
+    // Phone Number is always empty
+    let fakePhoneNumber = "9999999999"
+    guard let url = URL(string: "tel://\(fakePhoneNumber)") else { return }
+    UIApplication.shared.open(url, options: [:], completionHandler: nil)
   }
   
 }
--- RealmMapViewMVP/RestaurantDetailsViewController.swift
+++ RealmMapViewMVP/RestaurantDetailsViewController.swift
@@ -7,13 +7,40 @@
 //
 
 import UIKit
+import MapKit
 
 class RestaurantDetailsViewController: UIViewController {
   
+  // MARK: - DEPENDENCIES
+  
   var presenter: RestaurantDetailsPresenterRepresentable!
+  
+  // MARK: - IBOUTLETS
+  
+  @IBOutlet private weak var mapView: MKMapView!
+  @IBOutlet private weak var restaurantNameLabel: UILabel!
+  @IBOutlet private weak var restaurantAddressLabel: UILabel!
+  @IBOutlet private weak var restaurantCityLabel: UILabel!
+  @IBOutlet private weak var restaurantPhoneLabel: UILabel!
+  @IBOutlet private weak var restaurantRatingLabel: UILabel!
+  @IBOutlet private weak var phoneButton: UIButton!
+  
+  // MARK: - VIEW LIFE CYCLE
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    restaurantNameLabel.text = presenter.restaurantName
+    restaurantAddressLabel.text = presenter.restaurantAddress
+    restaurantCityLabel.text = presenter.restaurantCity
+    restaurantPhoneLabel.text = presenter.restaurantPhone
+    restaurantRatingLabel.text = presenter.restaurantRating
+    phoneButton.isEnabled = presenter.phoneEnabled
   }
   
+  // MARK: - IBACTIONS
+  
+  @IBAction func onPhonePressed(_ sender: UIButton) {
+    presenter.phonePressed()
+  }
+    
 }
